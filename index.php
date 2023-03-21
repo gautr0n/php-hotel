9fd801a1f4166db17228846801167404e2dbd062
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-03-21 17:58:03 +0100

    finished

--- index.php
+++ index.php
@@ -41,3 +41,42 @@ $hotels = [
 ];
 
 ?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container text-center">
+        <h1 class="text-danger">Hotels</h1>
+        <div class="row">
+            <?php
+                foreach($hotels as $hotel){
+            ?>
+            <div class="col">
+                <h2 class="text-primary"><?php echo $hotel['name']?></h2>
+                <div><?php echo $hotel['description']?></div>
+                <div>
+                    <?php 
+                        if($hotel['parking']){
+                            echo 'yes';
+                        } else{
+                            echo 'no';
+                        }
+                    ?>
+                </div>
+                <div><?php echo $hotel['vote']?></div>
+                <div><?php echo $hotel['distance_to_center']?></div>
+                    </div>
+            <?php
+                }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
